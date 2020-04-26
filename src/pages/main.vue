76295a1987cebd1672db2862dd2c2a5e006106a0
chendongpu <2323178881@qq.com>
2020-04-26 11:50:18 +0800

    默认Toast，带icon标志的Toast，自定义位置的Toast

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -11,5 +11,45 @@
   }
 </style>
 <script>
-  export default{}
+    import { Toast } from 'mint-ui';
+    export default {
+        name: 'Toast',
+        data(){
+            return {
+                examplename: "Toast"
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+            ToastA(){
+                Toast('默认Toast');
+            },
+            ToastB(){
+                Toast({
+                    message: '操作成功',
+                    iconClass: 'fa fa-check fa-2x'
+                });
+            },
+            ToastC(){
+                Toast({
+                    message: '自定义位置',
+                    position: 'bottom',
+                    duration: 5000
+                });
+            }
+        }
+    }
 </script>
+<style scoped>
+  ul {
+    padding-left: 10px;
+    padding-top: 20px;
+    padding-right: 10px;
+  }
+  li {
+    margin: 20px 0;
+    list-style: none;
+  }
+</style>
